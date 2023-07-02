a2861eeccff3ba3d8840027dc69c906e475d6906
Luis Felipe <luis.felipeszsv@gmail.com>
2023-07-02 14:25:58 -0300

    add aliases to fish

--- fish/config.fish
+++ fish/config.fish
@@ -2,5 +2,11 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+# Add starship
+starship init fish | source
+
 # Remove greeting
 set fish_greeting
+
+alias v="lvim"
+alias c="code"
