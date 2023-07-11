92dbcbd06ffa5648c4ece32e0eebaeeab976fdf2
Luis Felipe <luis.felipeszsv@gmail.com>
2023-07-10 22:49:04 -0300

    start using lsp zero

--- nvim/lua/luis/setup.lua
+++ nvim/lua/luis/setup.lua
@@ -13,11 +13,11 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup(
   {
-    "nvim-lua/plenary.nvim", --support for other packages
-    "nvim-treesitter/nvim-treesitter", --color highlingting
-    "christoomey/vim-tmux-navigator", --navigate between buffers and tmux
-    "nvim-telescope/telescope.nvim", --neovim fuzzy finder
-    "nvim-telescope/telescope-fzf-native.nvim", --faster fuzzy finder
+    "nvim-lua/plenary.nvim",                                     --support for other packages
+    "nvim-treesitter/nvim-treesitter",                           --color highlingting
+    "christoomey/vim-tmux-navigator",                            --navigate between buffers and tmux
+    "nvim-telescope/telescope.nvim",                             --neovim fuzzy finder
+    "nvim-telescope/telescope-fzf-native.nvim",                  --faster fuzzy finder
 
     { "catppuccin/nvim", name = "catppuccin", priority = 1000 }, --catpuccin theme
 
@@ -29,18 +29,31 @@ require("lazy").setup(
       },
     },
 
-    "williamboman/mason.nvim", --manager for lsp, formatters and linters
-    "williamboman/mason-lspconfig.nvim", --fullfill gap between nvim-lspconfig and mason
-    "neovim/nvim-lspconfig", --add lsp
-    "hrsh7th/nvim-cmp", --neovim autocompletion
-    "jose-elias-alvarez/null-ls.nvim", --use lsp to format code
-    "hrsh7th/cmp-buffer", --autocompeltion buffer
+    {
+      'VonHeikemen/lsp-zero.nvim',
+      branch = 'v2.x',
+      dependencies = {
+        -- LSP Support
+        { 'neovim/nvim-lspconfig' }, -- Required
+        {                            -- Optional
+          'williamboman/mason.nvim',
+          build = function()
+            pcall(vim.cmd, 'MasonUpdate')
+          end,
+        },
+        { 'williamboman/mason-lspconfig.nvim' }, -- Optional
+        -- Autocompletion
+        { 'hrsh7th/nvim-cmp' },                  -- Required
+        { 'hrsh7th/cmp-nvim-lsp' },              -- Required
+        { 'L3MON4D3/LuaSnip' },                  -- Required
+      },
+    },
 
-    "windwp/nvim-autopairs", --create pairs for <, {, [, (
-    "windwp/nvim-ts-autotag", --auto rename html tags
+    "windwp/nvim-autopairs",    --create pairs for <, {, [, (
+    "windwp/nvim-ts-autotag",   --auto rename html tags
 
     "terrortylor/nvim-comment", --auto comment
-    "lewis6991/gitsigns.nvim", --git integration
+    "lewis6991/gitsigns.nvim",  --git integration
 
     "akinsho/bufferline.nvim"
   }
