b851f1ec33f20fdc589d9f3d9039b0093b00f09e
Luis Felipe <luis.felipeszsv@gmail.com>
2023-07-11 00:31:05 -0300

    add lsp zero config file to nvim

---
+++ nvim/lua/luis/plugins/lsp/lsp-zero.lua
@@ -0,0 +1,56 @@
+local lsp = require('lsp-zero').preset({})
+
+lsp.on_attach(function(client, bufnr)
+  lsp.default_keymaps({ buffer = bufnr })
+end)
+
+-- (Optional) Configure lua language server for neovim
+require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
+
+lsp.setup()
+
+local cmp = require('cmp')
+
+local lsp_symbols = {
+  Text = "   (Text) ",
+  Method = "   (Method)",
+  Function = "   (Function)",
+  Constructor = "   (Constructor)",
+  Field = " ﴲ  (Field)",
+  Variable = "[] (Variable)",
+  Class = "   (Class)",
+  Interface = " ﰮ  (Interface)",
+  Module = "   (Module)",
+  Property = " 襁 (Property)",
+  Unit = "   (Unit)",
+  Value = "   (Value)",
+  Enum = " 練 (Enum)",
+  Keyword = "   (Keyword)",
+  Snippet = "   (Snippet)",
+  Color = "   (Color)",
+  File = "   (File)",
+  Reference = "   (Reference)",
+  Folder = "   (Folder)",
+  EnumMember = "   (EnumMember)",
+  Constant = " ﲀ  (Constant)",
+  Struct = " ﳤ  (Struct)",
+  Event = "   (Event)",
+  Operator = "   (Operator)",
+  TypeParameter = "   (TypeParameter)",
+}
+
+cmp.setup({
+  mapping = {
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ["<cr>"] = cmp.mapping.confirm({ select = true }),
+    ["<s-tab>"] = cmp.mapping.select_prev_item(),
+    ["<tab>"] = cmp.mapping.select_next_item(),
+  },
+  formatting = {
+    format = function(entry, item)
+      item.kind = lsp_symbols[item.kind]
+      item.menu = entry.source.name
+      return item
+    end,
+  },
+})
